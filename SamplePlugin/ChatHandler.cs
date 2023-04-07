30ce8045c7263583876101aa3a6079425f84854a
Junior Sohn <juniorsohn@hotmail.com>
2023-04-07 11:09:47 -0300

    initial commit

---
+++ SamplePlugin/ChatHandler.cs
@@ -0,0 +1,124 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Game.Gui;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Interface.Windowing;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using SamplePlugin.Windows;
+
+namespace SamplePlugin
+{
+    public sealed class ChatHandler : IDalamudPlugin
+    {
+        public string Name => "Sample Plugin";
+        private const string CommandName = "/voz";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        
+        public Configuration Configuration { get; init; }
+        public WindowSystem WindowSystem = new("SamplePlugin");
+
+        private ConfigWindow ConfigWindow { get; init; }
+        private MainWindow MainWindow { get; init; }
+        // Interface que dá pra puxar coisas tipo path do plugin e umas parada aí
+        private DalamudPluginInterface _pi; 
+        // nosso manipulador de chat
+        private ChatGui _chatGui; 
+        // sei pra que serve isso não
+        private Configuration config; 
+        
+        
+        public ChatHandler([RequiredVersion("1.0")] DalamudPluginInterface pluginInterface, [RequiredVersion("1.0")] ChatGui chat,
+                           [RequiredVersion("1.0")] CommandManager commands)
+        {
+            _pi = pluginInterface;
+            _chatGui = chat;
+            this.config = (Configuration)_pi.GetPluginConfig() ?? new Configuration();
+            this.config.Initialize(_pi);
+
+            _chatGui.ChatMessage += Chat_onChatMessage;
+            
+        
+
+
+
+
+
+
+
+
+
+            // you might normally want to embed resources and load them from the manifest stream
+            /*
+            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+            var ImagePathPepe = Path.Combine(pluginInterface.AssemblyLocation.Directory?.FullName!, "pepe.png");
+            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+            var pepeImage = PluginInterface.UiBuilder.LoadImage(ImagePathPepe);
+            ConfigWindow = new ConfigWindow(this);
+            MainWindow = new MainWindow(this, goatImage, pepeImage);
+            */
+            WindowSystem.AddWindow(ConfigWindow);
+            WindowSystem.AddWindow(MainWindow);
+
+            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+
+        private void Chat_onChatMessage(Dalamud.Game.Text.XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled)
+        {
+            if (config.Enabled)
+            {
+                foreach (var payload in message.Payloads)
+                {
+                    if (payload is TextPayload textPayload)
+                    {
+                        textPayload.Text = sendTextToApi(textPayload.Text);
+                    }
+                }    
+                
+            } 
+        }
+
+        private string? sendTextToApi(string text)
+        {
+            
+            return "ok!";
+        }
+        
+        
+        public void Dispose()
+        {
+            this.WindowSystem.RemoveAllWindows();
+            
+            ConfigWindow.Dispose();
+            MainWindow.Dispose();
+            
+            this.CommandManager.RemoveHandler(CommandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            MainWindow.IsOpen = true;
+        }
+
+        private void DrawUI()
+        {
+            this.WindowSystem.Draw();
+        }
+
+        public void DrawConfigUI()
+        {
+            ConfigWindow.IsOpen = true;
+        }
+    }
+}
---
+++ SamplePlugin/Windows/MainWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using ImGuiScene;
+using Lumina.Models.Materials;
+
+namespace SamplePlugin.Windows;
+
+public class MainWindow : Window, IDisposable
+{
+    private TextureWrap GoatImage;
+    private TextureWrap PepeImage;
+    private ChatHandler chatHandler;
+
+    public MainWindow(ChatHandler chatHandler, TextureWrap goatImage, TextureWrap pepeImage) : base(
+        "Nome da Janela", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        this.SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(800, 600),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        this.GoatImage = goatImage;
+        this.PepeImage = pepeImage;
+        this.chatHandler = chatHandler;
+    }
+
+    public void Dispose()
+    {
+        this.GoatImage.Dispose();
+    }
+
+    public override void Draw()
+    {
+        ImGui.Text($"The random config bool is {this.chatHandler.Configuration.Enabled}");
+        if(ImGui.Button("Botao de quem é top"))
+        {
+            this.chatHandler.DrawConfigUI();
+        }
+        if (ImGui.Button("Show Settings"))
+        {
+            this.chatHandler.DrawConfigUI();
+        }
+
+        ImGui.Spacing();
+        
+        ImGui.Text("Have a goat:");
+        ImGui.Indent(55);
+        ImGui.Image(this.GoatImage.ImGuiHandle, new Vector2(this.GoatImage.Width, this.GoatImage.Height));
+        ImGui.Unindent(55);
+        
+        ImGui.Text("And a pepe");
+        ImGui.Indent(55);
+        ImGui.Image(this.PepeImage.ImGuiHandle, new Vector2(PepeImage.Width, PepeImage.Height));
+        ImGui.Unindent(55);
+    }
+}
